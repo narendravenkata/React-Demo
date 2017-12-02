956138b3d0840f084f046be26953c2a4b41bfc40
Venkata Pratapaneni <vpratapaneni@Renkim.com>
2017-12-02 09:18:29 -0600

    updated App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          Hello Narendra. I am deleting all files before copying the new files into IIS Server
+          Hello Narendra. I am deleting all files before copying the new files into IIS Server. And I am using xcopy
         </p>
       </div>
     );
